882215a028c696d20e92d9a133a39403730c6bcc
jahongir136 <rasuljonov_96@naver.com>
2023-01-07 15:27:51 +0900

    [09] Callback functionla

--- train.js
+++ train.js
@@ -1,2 +1,33 @@
-//TASK
+
 console.log("train js ishga tushdi")
+const list = [
+    
+    "yaxshi talab bolling", //0-20
+    "togri boshliq tanlang va koproq hato qling", //20-30
+    "uzingizga ishlashingizni boshlang", //30-40
+    "siz kuchli bolgan narsalarni qiling", //40-50
+    "yoshlarga invistitsa qling", //50-60
+    "endi dam oling foydasi yoq", //60
+];
+
+function maslahatBering(a, callback) {
+    if (typeof a !== "number") callback("insert a number", null);
+    else if (a <= 20) callback(null, list[0]);
+    else if (a > 20 && a <= 30) callback(null, list[1]);
+    else if (a > 30 && a <= 40) callback(null, list[2]);
+    else if (a > 40 && a <= 50) callback(null, list[3]);
+    else if (a > 50 && a <= 60) callback(null, list[4]);
+    else {
+        setTimeout(function () {
+        callback(null, list[5]);
+        }, 5000);
+    }
+} 
+console.log("passed here 0")
+maslahatBering(10, (err, data) => {
+    if (err) console.log("ERROR", err);
+    else {
+        console.log("javob:", data);
+    }
+});
+console.log("passed here 1");
